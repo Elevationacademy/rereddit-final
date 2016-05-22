1aea8650562eb142b540a516ee4350cde74f8a81
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-05-22 14:58:54 +0300

    friend added now need to add user to friends profile as well

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/Users');
+
+router.param('user', function(req, res, next, id) {
+  console.log("hi");
+  var query = User.findById(id);
+
+  query.exec(function (err, user){
+    if (err) { return next(err); }
+    if (!user) { return next(new Error('can\'t find comment')); }
+
+    req.user = user;
+    return next();
+  });
+});
+
+router.put('/:user/addfriend', function(req, res, next) {
+  friend = req.body.friend;
+  if(!friend){
+    
+    req.user.friends.push(friend);
+
+    req.user.save(function(err, user) {
+      res.json(user);
+    });
+  } else {
+    return res.status(400).json({message: 'User is already a friend!'});
+  }
+
+  });
+
 module.exports = router;
