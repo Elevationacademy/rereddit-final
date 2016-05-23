b52a24b44d9440969a1b95bb249355237df75be9
Rony Rosenberg <ronyrosenberg@MacBook-Pro-de-Rony.local>
2016-05-23 15:22:07 +0300

    working now ok

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -52,16 +52,6 @@ app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $url
         }]
       }
     })
-    .state('profile',{
-      url:'/friendfinder',
-      templateUrl: '/templates/friendfinder.html',
-      controller: 'FriendFinderCtrl',
-      resolve:{
-        postPromise:['users', function(users){
-          return users.getAll();
-        }]
-      }
-    })
 
   $urlRouterProvider.otherwise('home');
 }]);
