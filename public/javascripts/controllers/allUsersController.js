e5d7a3a8ee18305bf29a518a304dc63415339e72
Brandon Bledsoe <brbledsoe.m@gmail.com>
2016-05-23 15:05:54 +0300

    trying to get the show hide on the add friend button to work

--- public/javascripts/controllers/allUsersController.js
+++ public/javascripts/controllers/allUsersController.js
@@ -1,12 +1,30 @@
 app.controller('AllUsersCtrl', ['$scope', 'users', 'auth',  function($scope, users, auth){
 
   $scope.users = users.users;
+  // console.log($scope.users[0].friends);
+  $scope.currentUser = auth.currentUser();
+  console.log($scope.currentUser.friends);
 
   $scope.addFriend = function (clickedUserId) {
 
-    var currentUserId = auth.currentUser()._id;
-    
-    users.addFriend(clickedUserId, currentUserId);
+    users.addFriend(clickedUserId, $scope.currentUser._id);
+
+  };
+
+  $scope.notFriend = function (friendId) {
+
+    for (var i = 0; i < $scope.currentUser.friends; i ++) {
+      
+      if( $scope.currentUser.friends[i] === friendId){
+        console.log($scope.currentUser.friends[i]);
+        return false;
+      }
+
+    }
+
+  };
+
+  $scope.Friend = function () {
 
   };
   
