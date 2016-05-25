38d9520a061d7adcfd891deea746fc3581b11597
Rony Rosenberg <ronyrosenberg@MacBook-Pro-de-Rony.local>
2016-05-25 10:23:22 +0300

    heroku with gulpexpress2

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,39 @@
-var gulp = require('gulp');
-var server = require('gulp-express');
+// var gulp = require('gulp');
+// var server = require('gulp-express');
 
-var connect = require('gulp-connect');
-var install = require('gulp-install');
+// var connect = require('gulp-connect');
+// var install = require('gulp-install');
 
  
-gulp.task('server', function () {
-    // Start the server at the beginning of the task 
-    server.run(['app.js']);
+// gulp.task('server', function () {
+//     // Start the server at the beginning of the task 
+//     server.run(['app.js']);
  
-    // Restart the server when file changes 
-    gulp.watch(['app/**/*.html'], server.notify);
-    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
-    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]); 
-    //Event object won't pass down to gulp.watch's callback if there's more than one of them. 
-    //So the correct way to use server.notify is as following: 
-    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
-        gulp.run('styles:css');
-        server.notify(event);
-        //pipe support is added for server.notify since v0.1.5, 
-        //see https://github.com/gimm/gulp-express#servernotifyevent 
-    });
+//     // Restart the server when file changes 
+//     gulp.watch(['app/**/*.html'], server.notify);
+//     gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+//     //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]); 
+//     //Event object won't pass down to gulp.watch's callback if there's more than one of them. 
+//     //So the correct way to use server.notify is as following: 
+//     gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
+//         gulp.run('styles:css');
+//         server.notify(event);
+//         //pipe support is added for server.notify since v0.1.5, 
+//         //see https://github.com/gimm/gulp-express#servernotifyevent 
+//     });
  
-    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-    gulp.watch(['app/images/**/*'], server.notify);
-    gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
-});
+//     gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+//     gulp.watch(['app/images/**/*'], server.notify);
+//     gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
+// });
  
-gulp.task('connect', function() {
-  connect.server();
-});
+// gulp.task('connect', function() {
+//   connect.server();
+// });
 
-gulp.task('install', function () {
-  gulp.src(['./package.json'])
-  .pipe(install());
-});
+// gulp.task('install', function () {
+//   gulp.src(['./package.json'])
+//   .pipe(install());
+// });
 
-gulp.task('default', ['install', 'connect']);
+// gulp.task('default', ['install', 'connect']);
